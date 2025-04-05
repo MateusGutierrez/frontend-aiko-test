be9ac495f0e306bcfa1745e744e41fce61086c3a
Mateus Gutierrez <118942632+MateusGutierrez@users.noreply.github.com>
2025-04-05 07:22:40 -0300

    add filter equipment

--- app/src/components/scroll-area.tsx
+++ app/src/components/scroll-area.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { IEquipment } from '@/zustand/interface';
 import { Accordion } from '@/components/ui/accordion';
 import ExpansibleCard from './expansible-card';
+import { Input } from './ui/input';
 
 interface Props {
   equipments: IEquipment[];
@@ -9,24 +11,34 @@ interface Props {
   activeId: string;
 }
 
-const ScrollAreaComponent: React.FC<Props> = ({
-  equipments,
-  setActiveId,
-  activeId
-}) => (
-  <ScrollArea className="h-[500px] w-[35%] rounded-md border">
-    <div className="p-4">
-      <Accordion type="single" collapsible className="w-full">
-        {equipments.map((equipment: IEquipment, index) => (
-          <ExpansibleCard
-            equipment={equipment}
-            setActiveId={setActiveId}
-            activeId={activeId}
-            key={index}
-          />
-        ))}
-      </Accordion>
-    </div>
-  </ScrollArea>
-);
+const ScrollAreaComponent = ({ equipments, setActiveId, activeId }: Props) => {
+  const [search, setSearch] = useState('');
+  const searchedList = search
+    ? equipments.filter(e =>
+        e.name.toLowerCase().includes(search.toLowerCase())
+      )
+    : equipments;
+  return (
+    <ScrollArea className="h-[500px] w-[35%] rounded-md border">
+      <div className="p-4 space-y-4">
+        <Input
+          placeholder="Buscar equipamento..."
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <Accordion type="single" collapsible className="w-full">
+          {searchedList.map(equipment => (
+            <ExpansibleCard
+              key={equipment.id}
+              equipment={equipment}
+              setActiveId={setActiveId}
+              activeId={activeId}
+            />
+          ))}
+        </Accordion>
+      </div>
+    </ScrollArea>
+  );
+};
+
 export default ScrollAreaComponent;
